e255ef61216c73937590faf438e1e1c46efe8f9b
Stalkero <chlebak112@interia.pl>
2023-02-23 11:57:52 +0100

    Update to net 4.8

--- aviatask/MainWindow.xaml.cs
+++ aviatask/MainWindow.xaml.cs
@@ -18,6 +18,8 @@ using Newtonsoft.Json;
 using Aviatask.CreateAccount;
 using System.Net.NetworkInformation;
 using System.Windows.Media.Animation;
+using System.Windows.UI.Notifications;
+using Microsoft.Toolkit.Uwp.Notifications;
 
 namespace Aviatask
 {
